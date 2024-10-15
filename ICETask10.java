5943b661bb8e56e62dc8243dc82c7f9cce445653
Mbali-24 <ST10447060@rcconnect.edu.za>
2024-10-15 22:57:33 +0200

    Add files via upload

---
+++ ICETask10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask10;
+
+/**
+ *
+ * @author macbookpro
+ */
+public class ICETask10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
